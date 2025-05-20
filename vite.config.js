45997a7b2c64180dca37469419cf8ed85e9aa867
briankim <brianhkim13@gmail.com>
2025-05-19 19:26:34 -0500

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     cors: true,
     proxy: {
-        '/api': 'http://localhost:8080',
-      },
+      '/api': 'http://localhost:8080',
     },
+  },
   build: {
-      outDir: "dist",
-      rollupOptions: {
-        input: {
-          main: "./index.html",
-          // Remove nested to fix the error
-        },
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.html",
       },
     },
   },
@@ -32,6 +29,5 @@ export default defineConfig({
         additionalData: `@import "@/styles/variables.scss";`,
       },
     },
-  }
+  },
 });
-
