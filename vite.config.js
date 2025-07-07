5d426651a52615bbd689aa500966487e0a90d4bc
briankim <brianhkim13@gmail.com>
2025-07-06 23:04:25 -0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist" // ✅ ensures build output is in /dist
+    outDir: "dist"
   },
   resolve: {
     alias: {
